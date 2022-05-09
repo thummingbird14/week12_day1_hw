f73269ef447194f0afd4227933eb3f255bcf6efe
thumminbird14 <tanyapurcell@hotmail.co.uk>
2022-05-09 16:57:14 +0100

    Commit toner functionality

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,8 +1,10 @@
 public class Printer {
     private int sheets;
+    private int toner;
 
-    public Printer(int sheets) {
+    public Printer(int sheets, int toner) {
         this.sheets = sheets;
+        this.toner = toner;
     }
 
     public int getSheets() {
@@ -13,10 +15,19 @@ public class Printer {
         this.sheets = sheets;
     }
 
+    public int getToner() {
+        return toner;
+    }
+
+    public void setToner(int toner) {
+        this.toner = toner;
+    }
+
     public void print(int pages, int copies){
         int used = pages * copies;
-        if (this.sheets >= used) {
+        if (this.sheets >= used && this.toner >= used) {
             setSheets(this.sheets - used);
+            setToner(this.toner - used);
         }
     }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -8,7 +8,7 @@ public class PrinterTest {
 
     @Before
     public void before(){
-        printer = new Printer(450);
+        printer = new Printer(450, 100);
     }
 
     @Test
@@ -17,15 +17,30 @@ public class PrinterTest {
     }
 
     @Test
+    public void PrinterHasToner(){
+        assertEquals(100, printer.getToner());
+    }
+
+    @Test
     public void PrinterWillPrint(){
         printer.print(2,50);
         assertEquals(350, printer.getSheets());
+        assertEquals(0, printer.getToner());
     }
+
 
     @Test
     public void PrinterWillNotPrintNotEnoughPaper(){
         printer.setSheets(100);
         printer.print(3,50);
         assertEquals(100, printer.getSheets());
+        assertEquals(100, printer.getToner());
+    }
+
+    @Test
+    public void PrinterWillNotPrintNotEnoughToner(){
+        printer.print(3,50);
+        assertEquals(450, printer.getSheets());
+        assertEquals(100, printer.getToner());
     }
 }
